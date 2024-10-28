aa466f23692d22f727fa152f6eb0557c8833325e
Fuad-Alkwkabani <fuadh4991@gmail.com>
2024-10-28 18:04:23 +0100

    creat the list FizzBuzz

--- src/main/java/fizzbuzz/App.java
+++ src/main/java/fizzbuzz/App.java
@@ -1,17 +1,9 @@
 package fizzbuzz;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(FizzBuzz.getFizzBuzz(i));
+        }
     }
 }
